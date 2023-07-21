cb308ce61feebb8434658653a035022a0e17162b
mkbarbara <makarena2001@yandex.ru>
2023-07-21 13:49:33 +0300

    Третье задание полностью готово

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,17 @@
 function compareArrays(arr1, arr2) {
-  
+	if (arr1.length !== arr2.length) {
+		return false;
+	}
+
+	const result = arr1.every((element, index) => element === arr2[index]);
+
+	return result;
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+	let result = users.filter(user => user.gender === gender)
+		.map(user => user.age)
+		.reduce((acc, age, _index, array) => acc + age / array.length, 0);
+
+	return result;
 }
